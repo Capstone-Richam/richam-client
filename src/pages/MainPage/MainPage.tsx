42d8e145e8881cc6771fe2386b451ebc4a7fa61d
ballsona0925@naver.com <ballsona0925@naver.com>
2023-12-08 17:11:08 +0900

    feat: add main page component

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,34 @@
+import { useRecoilValue } from "recoil";
+
+import MailList from "@/components/MailList";
+import Mail from "@/components/MailList/Mail";
+import { MailFilterAtom } from "@/recoil/atom/mail";
+import { MailFilterType } from "@/types";
+
+import * as styles from "./MainPage.style";
+import { maillistDummyData } from "./dummy";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const INFO: Record<any, { title: string; description: string }> = {
+  all: {
+    title: "전체",
+    description:
+      "당신의 모든 메일을 보여주고 있어요. 현재 저희 서비스는 구글과 네이버만 지원 중에요.",
+  },
+  naver: { title: "네이버", description: "당신의 네이버 메일이에요." },
+  google: { title: "구글", description: "당신의 구글 메일이에요." },
+};
+
+const MainPage = () => {
+  const mailFilter = useRecoilValue<MailFilterType>(MailFilterAtom);
+
+  return (
+    <styles.Container>
+      <styles.Title>{INFO[mailFilter].title} 메일</styles.Title>
+      <styles.Description>{INFO[mailFilter].description}</styles.Description>
+      <MailList data={maillistDummyData} />
+    </styles.Container>
+  );
+};
+
+export default MainPage;
---
+++ src/pages/MainPage/dummy.ts
@@ -0,0 +1,39 @@
+import { MailInfo } from "@/types";
+
+export const maillistDummyData: MailInfo[] = [
+  {
+    id: 1,
+    type: "naver",
+    title: "[넥슨] 새로운 환경에서 로그인 되었습니다.",
+    fromPerson: "no_replay@nexon.com",
+    date: "2023/12/08 17:02",
+  },
+  {
+    id: 2,
+    type: "google",
+    title: "1인가구에 '스마트초인종·가정용CCTV' 안심장비 지원",
+    fromPerson: "inews11@seoul.go.kr",
+    date: "2023/12/08 17:02",
+  },
+  {
+    id: 3,
+    type: "google",
+    title: "1인가구에 '스마트초인종·가정용CCTV' 안심장비 지원",
+    fromPerson: "inews11@seoul.go.kr",
+    date: "2023/12/08 17:02",
+  },
+  {
+    id: 4,
+    type: "google",
+    title: "1인가구에 '스마트초인종·가정용CCTV' 안심장비 지원",
+    fromPerson: "inews11@seoul.go.kr",
+    date: "2023/12/08 17:02",
+  },
+  {
+    id: 5,
+    type: "google",
+    title: "1인가구에 '스마트초인종·가정용CCTV' 안심장비 지원",
+    fromPerson: "inews11@seoul.go.kr",
+    date: "2023/12/08 17:02",
+  },
+];
