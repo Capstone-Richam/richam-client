9f75571d68c553f763925f14f1a9253ea771e6e9
ballsona0925@naver.com <ballsona0925@naver.com>
2023-12-09 01:22:36 +0900

    feat: install @tanstack/react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import routes from "./router";
@@ -5,11 +8,25 @@ import { GlobalStyle } from "./style/global";
 
 const App = () => {
   const router = createBrowserRouter(routes);
+
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnReconnect: false,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      {/*<Re initialIsOpen={false} />*/}
       <GlobalStyle />
       <RouterProvider router={router} />
-    </>
+    </QueryClientProvider>
   );
 };
 
