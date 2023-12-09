94bf1f27c6376852dd47e824a30a941915bf43d9
ballsona0925@naver.com <ballsona0925@naver.com>
2023-12-09 16:59:30 +0900

    feat: connect api related to keywords

--- src/api/query.ts
+++ src/api/query.ts
@@ -1,12 +1,23 @@
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 
-import { getMailListAysnc } from "./mail";
+import { MailFilterType } from "@/types";
+
+import { getFilteredMailListAsync, getMailDetailInfoAsync, getMailListAysnc } from "./mail";
 
 /** 메일 목록 조회 무한 스크롤 구현을 위한 hook */
-export const useGetInfiniteMailList = ({ type }: { type: "ALL" | "GOOGLE" | "NAVER" }) =>
+export const useGetInfiniteMailList = ({
+  type,
+  keywords, // 필터링 시
+}: {
+  type: MailFilterType;
+  keywords?: string[];
+}) =>
   useInfiniteQuery({
     queryKey: ["mail-list", type],
-    queryFn: ({ pageParam }) => getMailListAysnc({ type, page: pageParam?.page }),
+    queryFn: ({ pageParam }) =>
+      type !== "KEYWORD"
+        ? getMailListAysnc({ type, page: pageParam?.page })
+        : getFilteredMailListAsync({ keywords: keywords || [], page: pageParam?.page }),
     initialPageParam: { page: 0 },
     getNextPageParam: (lastPage) => {
       if (lastPage.hasNextPage) {
@@ -16,4 +27,11 @@ export const useGetInfiniteMailList = ({ type }: { type: "ALL" | "GOOGLE" | "NAV
       }
       return undefined;
     },
+  });
+
+export const useGetMailDetailInfo = ({ id }: { id: number }) =>
+  useQuery({
+    queryKey: ["mail-info", id],
+    queryFn: () => getMailDetailInfoAsync(id),
+    staleTime: Infinity, // 메일 내용은 업데이트될 일이 없으므로 서버에 다시 요청 안해도 됨.
   });
