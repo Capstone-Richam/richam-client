9dc335796595420f78a4486c98d5c49c2561422a
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-06-27 13:43:34 +0900

    Update baseURL

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from "axios";
 
 export const RICHAM = axios.create({
-  baseURL: "https://dev.richam.site/api",
+  baseURL: "https://kusitms28.store/api",
   headers: {
     "Content-Type": "application/json;charset=UTF-8",
   },
